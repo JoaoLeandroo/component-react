c6d060dc5c1069d14c6538ad0bdf3bb26abd013e
joaoleandro <new.joaoleandro@gmail.com>
2023-05-29 19:57:29 -0300

    primeiro commit, estudando sobre Components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Section from './components/Section'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <Section/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Section/index.jsx
@@ -0,0 +1,13 @@
+import '../Section/style.css'
+
+export default function Section() {
+    return(
+        <section>
+            <h2>Minha Sessão</h2>
+
+            <p>
+                Descrição conteúdo
+            </p>
+        </section>
+    )
+}
